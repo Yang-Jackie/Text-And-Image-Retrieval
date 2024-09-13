02ad8dc6c98a9f2aa3f1e8f254505e7da3498caa
Nguyen Nguyen Tuan <nguyenne.200507@gmail.com>
2024-09-13 22:52:46 +0700

    Changing the CLIP to open-clip for better retrieval

--- app.py
+++ app.py
@@ -1,62 +1,62 @@
-
 from flask import Flask, render_template, Response, request, send_file, jsonify
 import cv2
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import numpy as np
-import pandas as pd
-import glob 
-import json 
+import glob
+import json
 
 from utils.query_processing import Translation
 from utils.faiss import Myfaiss
 
-# http://0.0.0.0:5001/home?index=0
+EXTRACTED_PATH = "E:/aic2024/data/extracted"
 
-# app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder="templates")
 
-app = Flask(__name__, template_folder='templates')
-
-####### CONFIG #########
-with open('image_path.json') as json_file:
-    json_dict = json.load(json_file)
+with open("id2frames.json") as json_file:
+    json_list = json.load(json_file)
 
 DictImagePath = {}
-for key, value in json_dict.items():
-   DictImagePath[int(key)] = value 
+
+for item in json_list:
+    DictImagePath[int(item["_id"])] = EXTRACTED_PATH + '/' + item["path"]
 
 LenDictPath = len(DictImagePath)
-bin_file='faiss_normal_ViT.bin'
-MyFaiss = Myfaiss(bin_file, DictImagePath, 'cpu', Translation(), "ViT-B/32")
+bin_file = "faiss.bin"
+MyFaiss = Myfaiss(bin_file, DictImagePath, "cpu", Translation())
 ########################
 
-@app.route('/home')
-@app.route('/')
+
+@app.route("/home")
+@app.route("/")
 def thumbnailimg():
     print("load_iddoc")
 
     pagefile = []
-    index = request.args.get('index')
-    if index == None: index = 0
-    else: index = int(index)
+    index = request.args.get("index")
+    if index == None:
+        index = 0
+    else:
+        index = int(index)
 
     imgperindex = 100
-    
+
     # imgpath = request.args.get('imgpath') + "/"
     pagefile = []
 
     page_filelist = []
     list_idx = []
 
-    if LenDictPath-1 > index+imgperindex:
+    if LenDictPath - 1 > index + imgperindex:
         first_index = index * imgperindex
-        last_index = index*imgperindex + imgperindex
+        last_index = index * imgperindex + imgperindex
 
         tmp_index = first_index
         while tmp_index < last_index:
             page_filelist.append(DictImagePath[tmp_index])
             list_idx.append(tmp_index)
-            tmp_index += 1    
+            tmp_index += 1
     else:
         first_index = index * imgperindex
         last_index = LenDictPath
@@ -65,52 +65,59 @@ def thumbnailimg():
         while tmp_index < last_index:
             page_filelist.append(DictImagePath[tmp_index])
             list_idx.append(tmp_index)
-            tmp_index += 1    
+            tmp_index += 1
 
     for imgpath, id in zip(page_filelist, list_idx):
-        pagefile.append({'imgpath': imgpath, 'id': id})
+        pagefile.append({"imgpath": imgpath, "id": id})
 
-    data = {'num_page': int(LenDictPath/imgperindex)+1, 'pagefile': pagefile}
-    
-    return render_template('home.html', data=data)
+    data = {"num_page": int(LenDictPath / imgperindex) + 1, "pagefile": pagefile}
 
-@app.route('/imgsearch')
+    return render_template("home.html", data=data)
+
+
+@app.route("/imgsearch")
 def image_search():
     print("image search")
     pagefile = []
-    id_query = int(request.args.get('imgid'))
+    id_query = int(request.args.get("imgid"))
     _, list_ids, _, list_image_paths = MyFaiss.image_search(id_query, k=50)
 
-    imgperindex = 100 
+    imgperindex = 100
 
     for imgpath, id in zip(list_image_paths, list_ids):
-        pagefile.append({'imgpath': imgpath, 'id': int(id)})
+        pagefile.append({"imgpath": imgpath, "id": int(id)})
 
-    data = {'num_page': int(LenDictPath/imgperindex)+1, 'pagefile': pagefile}
-    
-    return render_template('home.html', data=data)
+    data = {"num_page": int(LenDictPath / imgperindex) + 1, "pagefile": pagefile}
 
-@app.route('/textsearch')
+    return render_template("home.html", data=data)
+
+
+@app.route("/textsearch")
 def text_search():
     print("text search")
 
     pagefile = []
-    text_query = request.args.get('textquery')
+    text_query = request.args.get("textquery")
     _, list_ids, _, list_image_paths = MyFaiss.text_search(text_query, k=50)
 
-    imgperindex = 100 
+    imgperindex = 100
 
     for imgpath, id in zip(list_image_paths, list_ids):
-        pagefile.append({'imgpath': imgpath, 'id': int(id)})
+        pagefile.append({"imgpath": imgpath, "id": int(id)})
 
-    data = {'query': text_query, 'num_page': int(LenDictPath/imgperindex)+1, 'pagefile': pagefile}
-    
-    return render_template('home.html', data=data)
+    data = {
+        "query": text_query,
+        "num_page": int(LenDictPath / imgperindex) + 1,
+        "pagefile": pagefile,
+    }
 
-@app.route('/get_img')
+    return render_template("home.html", data=data)
+
+
+@app.route("/get_img")
 def get_img():
     # print("get_img")
-    fpath = request.args.get('fpath')
+    fpath = request.args.get("fpath")
     # fpath = fpath
     list_image_name = fpath.split("/")
     image_name = "/".join(list_image_name[-2:])
@@ -121,17 +128,20 @@ def get_img():
         print("load 404.jpg")
         img = cv2.imread("./static/images/404.jpg")
 
-    img = cv2.resize(img, (1280,720))
+    img = cv2.resize(img, (1280, 720))
 
     # print(img.shape)
-    #img = cv2.putText(img, image_name, (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 4, cv2.LINE_AA)
+    # img = cv2.putText(img, image_name, (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 4, cv2.LINE_AA)
 
-    ret, jpeg = cv2.imencode('.jpg', img)
-    return  Response((b'--frame\r\n'
-                     b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n'),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    ret, jpeg = cv2.imencode(".jpg", img)
+    return Response(
+        (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + jpeg.tobytes() + b"\r\n\r\n"
+        ),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=1111)
--- utils/faiss.py
+++ utils/faiss.py
@@ -1,39 +1,50 @@
-from PIL import Image
 import faiss
 import matplotlib.pyplot as plt
 import math
-import numpy as np 
-import clip
+import numpy as np
+import open_clip
 from langdetect import detect
 
+
 class Myfaiss:
-    def __init__(self, bin_file : str,id2img_fps, device, translater, clip_backbone="ViT-B/32"):
-        self.index= self.load_bin_file(bin_file)
-        self.id2img_fps= id2img_fps
-        self.device= device
-        self.model, _ = clip.load(clip_backbone, device=device)
+    def __init__(
+        self,
+        bin_file: str,
+        id2img_fps,
+        device,
+        translater,
+        clip_backbone="ViT-SO400M-14-SigLIP-384",
+        clip_pretrained="webli",
+    ):
+        self.index = self.load_bin_file(bin_file)
+        self.id2img_fps = id2img_fps
+        self.device = device
+        self.model, _, self.preprocess = open_clip.create_model_and_transforms(
+            clip_backbone, pretrained=clip_pretrained, device=device
+        )
+        self.tokenizer = open_clip.get_tokenizer('ViT-SO400M-14-SigLIP-384')
         self.translater = translater
 
     def load_bin_file(self, bin_file: str):
         return faiss.read_index(bin_file)
-    
+
     def show_images(self, image_paths):
         fig = plt.figure(figsize=(15, 10))
         columns = int(math.sqrt(len(image_paths)))
-        rows = int(np.ceil(len(image_paths)/columns))
+        rows = int(np.ceil(len(image_paths) / columns))
 
-        for i in range(1, columns*rows +1):
-          img = plt.imread(image_paths[i - 1])
-          ax = fig.add_subplot(rows, columns, i)
-          ax.set_title('/'.join(image_paths[i - 1].split('/')[-3:]))
+        for i in range(1, columns * rows + 1):
+            img = plt.imread(image_paths[i - 1])
+            ax = fig.add_subplot(rows, columns, i)
+            ax.set_title("/".join(image_paths[i - 1].split("/")[-3:]))
 
-          plt.imshow(img)
-          plt.axis("off")
+            plt.imshow(img)
+            plt.axis("off")
 
         plt.show()
-        
-    def image_search(self, id_query, k): 
-        query_feats = self.index.reconstruct(id_query).reshape(1,-1)
+
+    def image_search(self, id_query, k):
+        query_feats = self.index.reconstruct(id_query).reshape(1, -1)
 
         scores, idx_image = self.index.search(query_feats, k=k)
         idx_image = idx_image.flatten()
@@ -41,16 +52,17 @@ class Myfaiss:
         infos_query = list(map(self.id2img_fps.get, list(idx_image)))
         image_paths = [info for info in infos_query]
 
-        
         return scores, idx_image, infos_query, image_paths
-    
+
     def text_search(self, text, k):
-        if detect(text) == 'vi':
+        if detect(text) == "vi":
             text = self.translater(text)
 
         ###### TEXT FEATURES EXACTING ######
-        text = clip.tokenize([text]).to(self.device)  
-        text_features = self.model.encode_text(text).cpu().detach().numpy().astype(np.float32)
+        text = self.tokenizer([text]).to(self.device)
+        text_features = (
+            self.model.encode_text(text).cpu().detach().numpy().astype(np.float32)
+        )
 
         ###### SEARCHING #####
         scores, idx_image = self.index.search(text_features, k=k)
